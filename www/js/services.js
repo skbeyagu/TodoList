7cfd655a960f5eef67083a32c5ea87ac4ef5c01c
Andrew Huffman <mahuffma@us.ibm.com>
2015-02-21 20:18:52 -0500

    Removed most views. Create list view and list factory with dummy data

--- www/js/services.js
+++ www/js/services.js
@@ -1,96 +1,56 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var chats = [{
+//TODO: Create cache
+.factory('TodoItems', function($q) {
+  var todoItemsList = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+    title: 'Todo List Item 0',
+    summary: 'Todo Item Summary 0',
+    dueDate: Date.now(),
+    isComplete: false
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+    title: 'Todo List Item 1',
+    summary: 'Todo Item Summary 1',
+    dueDate: Date.now(),
+    isComplete: false
   }, {
     id: 2,
-    name: 'Andrew Jostlin',
-    lastText: 'Did you get the ice cream?',
-    face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
+    title: 'Todo List Item 2',
+    summary: 'Todo Item Summary 2',
+    dueDate: Date.now(),
+    isComplete: false
   }, {
     id: 3,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+    title: 'Todo List Item 3',
+    summary: 'Todo Item Summary 3',
+    dueDate: Date.now(),
+    isComplete: false
   }, {
     id: 4,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
+    title: 'Todo List Item 4',
+    summary: 'Todo Item Summary 4',
+    dueDate: Date.now(),
+    isComplete: false
   }];
 
   return {
     all: function() {
-      return chats;
+      return $q.when(todoItemsList);
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
+    remove: function(itemId) {
+      todoItemsList = todoItemsList.filter(function(item){
+        return item.id != itemId;
+      });
+      return $q.when(true);
     },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    get: function(itemId) {
+      for (var i = 0; i < todoItemsList.length; i++) {
+        if (todoItemsList[i].id === parseInt(itemId)) {
+          return todoItemsList[i];
         }
       }
       return null;
-    }
-  }
-})
-
-/**
- * A simple example service that returns some data.
- */
-.factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var friends = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    notes: 'Enjoys drawing things',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    notes: 'Odd obsession with everything',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Andrew Jostlen',
-    notes: 'Wears a sweet leather Jacket. I\'m a bit jealous',
-    face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
-  }, {
-    id: 3,
-    name: 'Adam Bradleyson',
-    notes: 'I think he needs to buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 4,
-    name: 'Perry Governor',
-    notes: 'Just the nicest guy',
-    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
-  }];
-
-
-  return {
-    all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
     }
   }
 });
